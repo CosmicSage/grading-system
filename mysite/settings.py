2c4c1b7de8c4a7825493ad5b20ee1d6c1572f6a0
CosmicSage <prabhakarmhw@gmail.com>
2020-11-16 22:56:31 +0530

    grading init

--- mysite/settings.py
+++ mysite/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'flights.apps.FlightsConfig'
+    'flights.apps.FlightsConfig',
+    'grading.apps.GradingConfig',
 ]
 
 MIDDLEWARE = [
