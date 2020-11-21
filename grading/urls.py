8f87f11a97ad300e31b61eba1171ab6742211d84
CosmicSage <prabhakarmhw@gmail.com>
2020-11-21 22:15:16 +0530

    Basic Registration handling

--- grading/urls.py
+++ grading/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 # urls
 urlpatterns = [
-    path("", views.index)
+    path("", views.index, name="home"),
+    path("login", views.login, name="login"),
+    path("logout", views.logout, name="logout"),
+    path("register", views.register, name="register"),
+    path("account/<str:type>", views.account, name="account"),
 ]
--- grading/views.py
+++ grading/views.py
@@ -1,6 +1,47 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
 from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+def index(request):
+    return render(request, "grading/index.html")
+
+def login(request):
+    return HttpResponse("Sda")
+
+def logout(request):
+    return HttpResponse("DOSILGJAOEW")
+
+def account(request, type):
+    if type not in ['s', 't']:
+        return CustomHttpResponse(code=511)
+    context = dict(
+        type="Student" if type == "s" else "Teacher",
+        form=UserCreationForm
+    )
+    return render(request, "grading/register.html", context)
+
+def register(request):
+    # print(dir(request), request.path, request.get_raw_uri(), request.headers)
+    import json
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        print(f"{dir(form)}\n\n, {form.data},\n\n{dir(form.hidden_fields)}\n\n{dir(form.errors)}\n\n{form.full_clean}\n\n{form.error_messages}")
+        if form.is_valid():
+            pass
+        else:
+            context = dict(message=form.error_messages)
+            return HttpResponseRedirect(reverse('account', args=(form.data["type"][0].lower()[0],)))
+
+        # try:
+        #     if request.body["type"].lower()[0] in ['s', 't']:
+        #         print(request.body)
+        # except (KeyError, IndexError) as e:
+        #     return CustomHttpResponse(code=409)
+        # print(request.body)
+        return HttpResponse(json.dumps(dict(go="bo")), content_type="application/json")
+    return CustomHttpResponse(code=405)
 
 from http import HTTPStatus
 from django.http import HttpResponse
@@ -28,22 +69,3 @@ class CustomHttpResponse(HttpResponse):
 
 # class HttpResponseNoContent(HttpResponse):
 #     status_code = HTTPStatus.NO_CONTENT
-
-# Create your views here.
-def index(request):
-    return render(request, "grading/index.html")
-
-def login(request):
-    return HttpResponse("Sda")
-
-def logout(request):
-    return HttpResponse("DOSILGJAOEW")
-
-def account(request, type):
-    if type not in ['s', 't']:
-        return CustomHttpResponse(code=511)
-    context = dict(
-        type="Student" if type == "s" else "Teacher",
-        form=UserCreationForm
-    )
-    return render(request, "grading/register.html", context)
