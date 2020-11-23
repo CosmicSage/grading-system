5dc00f7cbb5a81232c1abfb9517f925325fa14b5
CosmicSage <prabhakarmhw@gmail.com>
2020-11-23 18:50:26 +0530

    Account defined

--- grading/models.py
+++ grading/models.py
@@ -1,3 +1,10 @@
-from django.db import models
-
+from django.db import models as m
+from django.contrib.auth.models import User
 # Create your models here.
+class Account(m.Model):
+    user = m.OneToOneField(User, on_delete=m.CASCADE, related_name="profile")
+    is_teacher = m.BooleanField(default=False)
+    is_student = m.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username}"
