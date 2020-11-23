163adab2a441ae065b119cb0a7b8b3b8fde2c0de
CosmicSage <prabhakarmhw@gmail.com>
2020-11-23 23:47:16 +0530

    Premature Business Model

--- grading/models.py
+++ grading/models.py
@@ -1,5 +1,9 @@
 from django.db import models as m
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+ALPHANUMERIC = "abcdefghjkmnpqrstuvwxyz01234567889"
+
 # Create your models here.
 class Account(m.Model):
     user = m.OneToOneField(User, on_delete=m.CASCADE, related_name="profile")
@@ -8,3 +12,23 @@ class Account(m.Model):
 
     def __str__(self):
         return f"{self.user.username}"
+
+class Responder(m.Model):
+    work = m.FileField(blank=True)
+    student = m.ForeignKey(Account, on_delete=m.CASCADE, related_name="responder")
+    score = m.IntegerField(default=1, validators=[MaxValueValidator(100), MinValueValidator(1)], blank=True)
+
+    def __str__(self):
+        return f"{self.student.user.username}"
+
+class Assignment(m.Model):
+    title = m.CharField(max_length=69)
+    description = m.CharField(max_length=640)
+    code = m.CharField(default=User.objects.make_random_password(length=6, allowed_chars=ALPHANUMERIC), max_length=6)
+    teacher = m.OneToOneField(Account, on_delete=m.CASCADE, related_name="grader")
+    # students = m.ManyToManyField(Account, related_name="responders", blank=True)
+    # responses = m.ManyToManyField(Response, related_name="answers", blank=True)
+    responders = m.ManyToManyField(Responder, related_name="volunteer", blank=True)
+
+    def __str__(self):
+        return f"{self.title}"
