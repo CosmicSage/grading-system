3b590c874d583ae9f9dce2c07bb98e7bc466582e
CosmicSage <prabhakarmhw@gmail.com>
2020-11-24 14:42:44 +0530

    Assignment Urls

--- grading/urls.py
+++ grading/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.urls import include
 # urls
+
 urlpatterns = [
     path("", views.index, name="home"),
     path("login", views.login, name="login"),
@@ -9,5 +10,6 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("account/<str:type>", views.account, name="account"),
     # path("accounts/", include("django.contrib.auth.urls"))
-    path("assignments", views.assignments, name="assignments")
+    path("assignments", views.assignments, name="assignments"),
+    path("assignments/a/<str:type>", views.a, name='a')
 ]
